1a98786cbfc43e3a5fb13d87950bc905bb41cb07
DylanLange <dylan_lange@live.com>
2016-08-20 16:50:49 +1200

    added some playlist searching functionality

--- routes/index.js
+++ routes/index.js
@@ -24,12 +24,24 @@ router.get('/auth', function (req, res) {
 });
 
 router.get('/callback', function (req, res) {
-  var key = req.query.code;
-  //res.send(key);
-  post(key);
+    var key = req.query.code;
+    //res.send(key);
+    post(key, res);
 });
 
-function post(key) {
+router.get('/success', function (req, res) {
+    res.send("Success baby");
+    spotifyApi.getPlaylist('1281597756', '1ok2P5ointA9iZqPGQGSLQ')
+        .then(function(data) {
+            for (i in data.body.tracks.items){
+                console.log(i.track);
+            }
+        }, function(err) {
+            console.error(err);
+        });
+});
+
+function post(key, res) {
   //var encoded_auth = Base64.toByteArray('5b3fe858f8074daba7c88c148532de17:18fe72f57cc34431bdd84e513539cb37');
   var encoded_auth = (new Buffer("5b3fe858f8074daba7c88c148532de17:18fe72f57cc34431bdd84e513539cb37").toString("base64"));
   request.post(
@@ -46,7 +58,10 @@ function post(key) {
       },
       function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          console.log(body);
+            var token = JSON.parse(body).access_token;
+            console.log(token);
+            spotifyApi.setAccessToken(token);
+            res.redirect("http://localhost:3000/success");
         } else {
           console.log(body);
           console.log(response.statusCode);
